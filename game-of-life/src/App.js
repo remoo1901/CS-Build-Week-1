cff98e0658ac592ffcdac9e0dde719d0e2b45cbe
Remon Abdou <remoo1901@hotmail.com>
2020-09-16 21:47:40 -0400

    working on html structure for the game

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: [90, 20],
+    };
+
+    this.handleColumnChange = this.handleColumnChange.bind(this);
+    this.handleRowChange = this.handleRowChange.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.stopGame = this.stopGame.bind(this);
+    this.renderBoard = this.renderBoard.bind(this); 
+  }
+
+  handleRowChange(event) {}
+
+  handleColumnChange(event) {}
+
+  startGame() {}
+
+  stopGame() {}
+
+  runGame() {}
+
+  renderBoard() {}
+
+  render() {
+    return (
+      <div className="worldContainer">
+        <div className="headerContainer">
+          <div className="headerInnerContainer">
+
+            <label classname="label">
+              Row:
+              <input
+                className="input"
+                type="text"
+                value={this.state.size[1]}
+                onChange={this.handleRowChange}
+              />
+            </label>
+
+            <label classname="label">
+              Columns:
+              <input
+                className="input"
+                type="text"
+                value={this.state.size[0]}
+                onChange={this.handleColumnChange}
+              />
+
+            </label>
+
+          </div>
+          <div className="headerButtons">
+
+            <button className="submit" onClick={this.startGame}>
+              Start
+            </button>
+
+            <button className="submit" onClick={this.stopGame}>
+              Stop
+            </button>
+
+          </div>
+          Generation:
+        </div>
+
+        <div className="boardContainer">{this.renderBoard()}</div>
+      </div>
+    );
+  }
 }
-
-export default App;
