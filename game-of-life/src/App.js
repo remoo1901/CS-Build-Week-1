310b47f723defa806f2f885d9e1047426cac8e72
Remon Abdou <remoo1901@hotmail.com>
2020-09-17 19:43:11 -0400

    finished creating cells

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
+import Cell from "./Cell";
 import "./App.css";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      size: [90, 20],
+      size: [80, 20],
     };
 
     this.handleColumnChange = this.handleColumnChange.bind(this);
     this.handleRowChange = this.handleRowChange.bind(this);
     this.startGame = this.startGame.bind(this);
     this.stopGame = this.stopGame.bind(this);
-    this.renderBoard = this.renderBoard.bind(this); 
+    this.renderBoard = this.renderBoard.bind(this);
   }
 
   handleRowChange(event) {}
@@ -25,14 +26,29 @@ export default class App extends Component {
 
   runGame() {}
 
-  renderBoard() {}
+  renderBoard() {
+    let newWorld = [];
+    let cellRow = [];
+
+    for (let i = 0; i < this.state.size[0]; i++) {
+      for (let j = 0; j < this.state.size[1]; j++) {
+        cellRow.push(<Cell key={[i, j]} />);
+      }
+      newWorld.push(
+        <div className="row" key={i}>
+          {cellRow}
+        </div>
+      );
+      cellRow = [];
+    }
+    return newWorld;
+  }
 
   render() {
     return (
       <div className="worldContainer">
         <div className="headerContainer">
           <div className="headerInnerContainer">
-
             <label classname="label">
               Row:
               <input
@@ -51,12 +67,9 @@ export default class App extends Component {
                 value={this.state.size[0]}
                 onChange={this.handleColumnChange}
               />
-
             </label>
-
           </div>
           <div className="headerButtons">
-
             <button className="submit" onClick={this.startGame}>
               Start
             </button>
@@ -64,7 +77,6 @@ export default class App extends Component {
             <button className="submit" onClick={this.stopGame}>
               Stop
             </button>
-
           </div>
           Generation:
         </div>
---
+++ game-of-life/src/Cell.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+
+
+class Cell extends Component {
+  render() {
+    return (<div className="cellContainer"></div>)
+
+  }
+}
+
+
+export default Cell;
