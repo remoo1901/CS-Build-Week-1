1590d28acf751e9e64292e7c97ba3313a3445175
Remon Abdou <remoo1901@hotmail.com>
2020-09-20 00:02:25 -0400

    Updates

---
+++ precache-manifest.5cd0429a35a6370a90529a4907d7571d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ffa6b98a9f4dcc72786be3eb15c078",
+    "url": "/CS-Build-Week-1/index.html"
+  },
+  {
+    "revision": "caa7a830dc90b21cf62d",
+    "url": "/CS-Build-Week-1/static/css/main.c1befe2c.chunk.css"
+  },
+  {
+    "revision": "29fd12c0e7feddda8bb1",
+    "url": "/CS-Build-Week-1/static/js/2.30066891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CS-Build-Week-1/static/js/2.30066891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa7a830dc90b21cf62d",
+    "url": "/CS-Build-Week-1/static/js/main.6a8f1123.chunk.js"
+  },
+  {
+    "revision": "20c2ef359732a14a20e7",
+    "url": "/CS-Build-Week-1/static/js/runtime-main.26f9a69d.js"
+  }
+]);
